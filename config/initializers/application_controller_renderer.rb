ff2d91ab0d793abf8840ccf2b791e6fb5de28c5e
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-17 13:23:12 +0000

    [add]画像投稿機能　※shop_nameにnilエラーあり

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e78be2f7b2a8687520205dc4536f4a896eba283d57ea35fd1ffd57aa61fc22654297af1aa7be67ab0c0bebfd76399906743c0936e66ccdbc5d8d408bb9b998fa'
